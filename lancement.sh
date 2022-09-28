5452119e7eecb63d89c06f240b10f6450c3512a2
KonkArLab <107851441+KonkArLab@users.noreply.github.com>
2022-09-28 11:49:35 +0200

    Ajout du fichier ini et lancement
    De base le fichier .ini est le fichier de config et se place sur la racine de la clé USB. 
    Le fichier lancement.sh se met dans le répertoire courant et permet le lancement du script.

---
+++ lancement.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#Montage clef USB
+sudo mkdir /media/pi/00clef
+sudo mount /dev/sda1 /media/pi/00clef
+sudo chmod ugo+rwx /media/pi/00clef
+
+#RTC Mise à jour de l'heure
+sudo echo ds3231 0x68 > /sys/class/i2c-adapter/i2c-1/new_device
+#sudo hwclock -s
+
+#Lancement programme principal de la caméra dés l'allumage du KOSMOS ( mettre en commentaire si on veut le démarrer depuis le terminal )
+cd /home/pi/kospython
+python3 kosmos_main.py
