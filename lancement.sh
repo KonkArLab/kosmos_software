d27cfb7773f4c1c2053c09e5bd445ab800da56bf
AmauMaill <amaury.maill@gmail.com>
2022-10-28 01:58:25 +0200

    Use dotenv, dynamic routes

--- lancement.sh
+++ lancement.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
+#Dotenv
+set -a; source .env; set +a
+
 #Montage clef USB
-sudo mkdir /media/pi/00clef
-sudo mount /dev/sda1 /media/pi/00clef
-sudo chmod ugo+rwx /media/pi/00clef
+sudo mkdir ${USB_ROOT_PATH}/00clef
+sudo mount /dev/sda1 ${USB_ROOT_PATH}/00clef
+sudo chmod ugo+rwx ${USB_ROOT_PATH}/00clef
 
 #RTC Mise à jour de l'heure
 sudo echo ds3231 0x68 > /sys/class/i2c-adapter/i2c-1/new_device
 #sudo hwclock -s
 
 #Lancement programme principal de la caméra dés l'allumage du KOSMOS ( mettre en commentaire si on veut le démarrer depuis le terminal )
-cd /home/pi/kospython
-python3 /kosmosV3-env/kosmos_main.py
+cd ~/kosmos_software
+source env/bin/activate # Sécurité
+python3 main.py
