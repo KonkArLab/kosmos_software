eb66f5d9539607fa1debb60f6c1f9c4d01c89c83
AmauMaill <amaury.maill@gmail.com>
2022-11-15 17:43:14 +0100

    Add if guards + echo statements

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Install Kosmos-config"
+echo "Begin install Kosmos-config"
 
 # Installer les mises à jour de l'OS
 sudo apt update -y 
@@ -9,12 +9,19 @@ sudo apt upgrade -y
 # Installer des "dependencies"
 sudo apt install python3-pip python3-venv pigpio -y
 
-# Création de l'environnement virtuel
-python3 -m venv env
-source env/bin/activate # Activation
+if [ ! -d env ]; then
+    echo "Create Python virtuel env"
+    # Création de l'environnement virtuel
+    python3 -m venv env
+fi
 
-# Installation des packages
-pip install -r requirements.txt
+if [ -d env ]; then
+    echo "Install Python libraries..."
+    # Activation de l'environnement virtuel
+    source env/bin/activate
+    # Installation des packages
+    pip install -U -r requirements.txt
+fi
 
 echo "End install Kosmos-config"
-reboot
+#reboot
