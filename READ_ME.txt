1de6ec92be32555c146fbc1457ff89221d132f03
ofauvarque <111113632+ofauvarque@users.noreply.github.com>
2023-09-18 11:29:16 +0200

    Add files via upload

--- READ_ME.txt
+++ READ_ME.txt
@@ -1,12 +1,14 @@
-#raspi config camera enabled
-sudo raspi-config
-
 #Télécharger la branche "kosmos_software" dans le fichier se trouvant dans "home"
 #Lien du git : https://github.com/KonkArLab/kosmos_software
+#Inserer une cle USB
 ____________________________________________________________________________________
 #Ouvir un terminal et taper :
 cd kosmos_software
 sudo chmod 755 install.sh
 sh install.sh
-____________________________________________________________________________________
+
+sudo raspi-config
+# 3 Interface options
+# I1 Legacy Camera -> enable
+_________________________________________________________________________________
 #La raspberry va redemarrer automatiquement et sera prete a etre utilisé
--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ sudo apt update
 
 #Installation du dernier OS
 sudo apt upgrade
+sudo apt autoremove
 
 #Telechargement de python 3
 sudo apt install python3-pip
@@ -17,12 +18,21 @@ sudo pip install -r requirements.txt
 nom_raspberry=$(whoami)
 echo "$nom_raspberry"
 
+#Recuperation du nom de la clef USB
+USB_NAME=$(lsblk -o LABEL,MOUNTPOINT | grep "/media\|/mnt" | awk '{print $1}')
+echo "$USB_NAME"
+
 #Creation du fichier de lancement
 cd ..
 echo "#!/bin/bash" > lancement.sh
 
 #Ajout de la commande de lancement du programme
 sudo echo "sleep 15
+
+#Deplacement du fichier kosmos_config.ini dans la cle USB
+sudo cp -n /home/$nom_raspberry/kosmos_software/kosmos_config.ini /media/$nom_raspberry/$USB_NAME
+
+#Lance kosmos_main.py 
 cd /home/$nom_raspberry/kosmos_software/kosmosV3-env
 sudo python3 kosmos_main.py" >> lancement.sh
 
@@ -32,14 +42,10 @@ sudo chmod 755 lancement.sh
 #Activation de "Legacy camera" et "i2c"
 cd ..
 sudo raspi-config nonint do_i2c 0
-#sudo raspi-config nonint do_camera 0
 
 #Ajout de la ligne de commande dans crontab qui permet le lancement au demarrage et création d'un dossier log
 mkdir -p /home/$nom_raspberry/kosmos_software/logfile
 (sudo crontab -l; echo "@reboot sudo bash /home/$nom_raspberry/kosmos_software/lancement.sh > /home/$nom_raspberry/kosmos_software/logfile/log.txt 2>&1";) | uniq - | sudo crontab
 sudo crontab -l
-
-#Redémareage de la raspberry
-sudo reboot
 
 exit 0
