c2367bd304fa333d30738065c12ef85da27b6d19
ofauvarque <111113632+ofauvarque@users.noreply.github.com>
2024-04-16 14:29:22 +0200

    Delete lancement.sh

--- lancement.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sleep 20
-
-#Deplacement du fichier kosmos_config.ini dans la cle USB
-#sudo cp -n /home/kosmos/kosmos_software/kosmos_config.ini /media/kosmos/kosmoscle2
-
-#Lance kosmos_main.py 
-cd /home/kosmos/kosmos_software/kosmosV3-env
-sudo python3 kosmos_main.py
