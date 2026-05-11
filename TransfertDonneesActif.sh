d1397cc9c246b3d4302e58cd8511e0c3696c92c7
ofauvarque <111113632+ofauvarque@users.noreply.github.com>
2026-05-11 16:09:03 +0200

    Update TransfertDonneesActif.sh

--- TransfertDonneesActif.sh
+++ TransfertDonneesActif.sh
@@ -1,8 +1,8 @@
-sudo nmcli connection modify "Wired connection 1" \
+sudo nmcli connection modify "EthernetPort" \
 ipv4.addresses 192.168.10.2/24 \
 ipv4.gateway 192.168.10.1 \
 ipv4.dns 192.168.10.1 \
 ipv4.method manual
 
-sudo nmcli connection down "Wired connection 1"
-sudo nmcli connection up "Wired connection 1"
+sudo nmcli connection down "EthernetPort"
+sudo nmcli connection up "EthernetPort"
