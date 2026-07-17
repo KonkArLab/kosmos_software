9017acc23ea3e88841368de3801fe500dded2b10
ofauvarque <111113632+ofauvarque@users.noreply.github.com>
2026-07-17 15:54:37 +0200

    Update kosmos_backend.py

--- kosmos-env/kosmos_backend.py
+++ kosmos-env/kosmos_backend.py
@@ -188,10 +188,14 @@ class Server:
             LONG = "ERR"
         # TP
         try:
-            press = self.myMain.thread_camera.pressure_sensor.pressure()
-            PRESSURE = f'{press:.1f}'
-            temp = self.myMain.thread_camera.pressure_sensor.temperature()  # Default is degrees C (no arguments)
-            TEMPERATURE = f'{temp:.1f}'
+            if self.myMain.thread_camera.pressure_sensor.read():
+                press = self.myMain.thread_camera.pressure_sensor.pressure()
+                PRESSURE = f'{press:.1f}'
+                temp = self.myMain.thread_camera.pressure_sensor.temperature()  # Default is degrees C (no arguments)
+                TEMPERATURE = f'{temp:.1f}'
+            else:
+                PRESSURE = "ERR"
+                TEMPERATURE = "ERR"
         except:
             PRESSURE = "ERR"
             TEMPERATURE = "ERR"
